a39ea0fd25ae213a1494af5b65a77e7630f15aac
Ornella-e <orneescaladacorsi@gmail.com>
2022-07-19 20:01:08 +0200

    user model ranking model

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,7 +4,7 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
-// You put the next routes here 👇
-// example: router.use("/auth", authRoutes)
+
+ router.use("/auth", authRoutes)
 
 module.exports = router;
