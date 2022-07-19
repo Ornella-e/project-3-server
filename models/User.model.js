1b1dce582975335bf52f4ae8d958bf809f2c5276
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-07-19 20:37:31 +0200

    couch-model

--- models/User.model.js
+++ models/User.model.js
@@ -1,16 +1,22 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
     username: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      unique: true 
     },
-    password: String,
-  },
+  
+  couch: [{
+    type: Schema.Types.ObjectId,
+    ref: "Couch"
+}],
+location: {
+  city: String,
+  country: String
+},
+},
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
 );
