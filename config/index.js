ea7613ff78b4286a10a23ff382f6eb943cc5c96c
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-08-16 19:56:41 +0200

    update

--- config/index.js
+++ config/index.js
@@ -18,7 +18,7 @@ module.exports = (app) => {
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
   // Services like heroku use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
-
+  console.log( process.env.MONGODB_URI)
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
